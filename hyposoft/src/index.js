f783409fea0e8f3a512cf411b5d3a10a6fc6e400
Anshu Dwibhashi <ad353@duke.edu>
2020-01-18 23:33:18 -0500

    Created settings page

--- hyposoft/src/index.js
+++ hyposoft/src/index.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import HomeScreen from './screens/HomeScreen'
-import DashboardScreen from './screens/DashboardScreen'
 import * as serviceWorker from './serviceWorker'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './utils/firebaseutils'
+
+import HomeScreen from './screens/HomeScreen'
+import DashboardScreen from './screens/DashboardScreen'
+import SettingsScreen from './screens/SettingsScreen'
 
 ReactDOM.render((
             <BrowserRouter>
                 <Switch>
                     <Route exact path='/' component={HomeScreen} />
                     <Route exact path='/dashboard' component={DashboardScreen} />
+                    <Route exact path='/settings' component={SettingsScreen} />
                 </Switch>
             </BrowserRouter>
         ), document.getElementById('root'))
