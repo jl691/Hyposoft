94f68a32db516c204313696a553d49bf517a4956
Janice Liu <janice.jy.liu@gmail.com>
2020-01-20 15:14:38 -0500

    Changed Instances from accordion to datatable #16

--- hyposoft/src/components/InstanceTable.js
+++ hyposoft/src/components/InstanceTable.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-import { Accordion, AccordionPanel, Box, Text, List} from 'grommet'
+import { DataTable, Box, Text } from 'grommet'
 import InstanceRow from './InstanceRow';
 import * as instutils from '../utils/instanceutils'
 
@@ -21,44 +21,74 @@ export default class InstanceTable extends Component {
     }
    
     render() {
-        return this.state.instances.map(( instance ) => (
-            <Accordion>
-                <AccordionPanel 
-                key={instance.id}
-                label={ instance.model}>
-                <Box pad="medium" background="light-2">
-          
+        // return this.state.instances.map(( instance ) => 
+        // (
+            // <Accordion>
+            //     <AccordionPanel 
+            //     key={instance.id}
+            //     label={ instance.model}>
+            //     <Box pad="medium" background="light-2">
+        
+            //     <List
+            //     primaryKey="category"
+            //     secondaryKey="value"
+            //     data={[
+            //     { category: "Hostname", value: instance.hostname},
+            //     { category: "Rack", value: instance.rack},
+            //     { category: "RackU", value: instance.rackU},
+            //     { category: "Owner", value: instance.owner},
+            //     { category: "Comment", value: instance.comment},
 
-                <List
-                primaryKey="category"
-                secondaryKey="value"
-                data={[
-                { category: "Hostname", value: instance.hostname},
-                { category: "Rack", value: instance.rack},
-                { category: "RackU", value: instance.rackU},
-                { category: "Owner", value: instance.owner},
-                { category: "Comment", value: instance.comment},
+            //     ]}
+            //     />
+            //     </Box>
+            //     </AccordionPanel>  
+            // </Accordion>
 
+            return(
+            <DataTable
+                columns={[
+                 {
+                 property: 'doc_id',
+                header: <Text>ID</Text>,
+                primary: true,
+                    },
+                    {
+                    property: 'model',
+                   header: <Text>Model</Text>,
+             
+                   },
+                   {
+                    property: 'hostname',
+                   header: <Text>Hostname</Text>,
+           
+                   },
+                   {
+                    property: 'rack',
+                   header: <Text>Rack</Text>,
+                 
+                   },
+                   {
+                    property: 'rackU',
+                   header: <Text>RackU</Text>,
+                  
+                   },
+                   {
+                    property: 'owner',
+                   header: <Text>Owner</Text>,
+              
+                   },
+                   {
+                    property: 'comment',
+                   header: <Text>Comment</Text>,
+                
+                   },
                 ]}
-                />
-                </Box>
-                </AccordionPanel>  
-            </Accordion>
+                data= {this.state.instances}
+                                   
+            />
  
-         ));
-        // console.log(this.state.instances.id)
-        // return (
-        //     <Accordion>
-        //          <AccordionPanel 
-        //         // label={ this.state.instances.model }>
-        //         label='Bitch'>
-        //         <Box pad="medium" background="light-2">
-        //         <Text>{ this.state.instances }</Text>
-        //         </Box>
-        //         </AccordionPanel>  
-        //     </Accordion>
-        //     //<h3>Hello</h3>
-        // )
+         );
       
     }
 }
