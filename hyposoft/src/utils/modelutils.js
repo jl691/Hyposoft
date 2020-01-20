37877a0d25b15da000388b414abd6fba69886448
Justin Havas <jah135@duke.edu>
2020-01-20 14:03:11 -0500

    Fixed doesModelDocExist and getSuggestedVendors functions #1 #2

--- hyposoft/src/utils/modelutils.js
+++ hyposoft/src/utils/modelutils.js
@@ -25,42 +25,43 @@ function createModel(vendor, modelNumber, height, displayColor, ethernetPorts, p
 }
 
 function modifyModel(vendor, modelNumber, height, displayColor, ethernetPorts, powerPorts, cpu, memory, storage, comment) {
-    firebaseutils.modelsRef.doc(combineVendorAndModelNumber(vendor, modelNumber)).update(packageUser(vendor, modelNumber, height, displayColor, ethernetPorts, powerPorts, cpu, memory, storage, comment))
+    firebaseutils.modelsRef.doc(combineVendorAndModelNumber(vendor, modelNumber)).update(packageModel(vendor, modelNumber, height, displayColor, ethernetPorts, powerPorts, cpu, memory, storage, comment))
 }
 
 function deleteModel(vendor, modelNumber) {
     firebaseutils.modelsRef.doc(combineVendorAndModelNumber(vendor, modelNumber)).delete()
 }
 
-function doesModelDocExist(vendor, modelNumber) {
+function doesModelDocExist(vendor, modelNumber, callback) {
     var docRef = firebaseutils.modelsRef.doc(combineVendorAndModelNumber(vendor, modelNumber))
     docRef.get().then(doc => {
-      // not sure what I should be returning
-      return doc.exists
+      callback(doc.exists)
     })
     .catch( error => {
       console.log("Error getting documents: ", error)
+      callback(null)
     })
 }
 
-function getSuggestedVendors(userInput) {
+function getSuggestedVendors(userInput, callback) {
   // https://stackoverflow.com/questions/46573804/firestore-query-documents-startswith-a-string/46574143
     var beginningInput = userInput.slice(0,userInput.length-1)
     var endInput = userInput.slice(userInput.length-1,userInput.length)
     var upperBound = beginningInput + String.fromCharCode(endInput.charCodeAt(0)+1)
     var query = firebaseutils.modelsRef.where("vendor",">=",userInput).where("vendor","<",upperBound)
 
-    var vendorArray = new Array()
+    var vendorArray = []
     query.get().then(querySnapshot => {
       querySnapshot.forEach( doc => {
-        if (!vendorArray.includes(doc.vendor)) {
-          vendorArray.push(doc.vendor)
+        if (!vendorArray.includes(doc.data().vendor)) {
+          vendorArray.push(doc.data().vendor)
         }
       })
-      return vendorArray
+      callback(vendorArray)
     })
     .catch( error => {
       console.log("Error getting documents: ", error)
+      callback(null)
     })
 }
 
