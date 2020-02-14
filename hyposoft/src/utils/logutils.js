8bea828411753ffc7e97f96c7b1303fa127c8d24
Justin Havas <jah135@duke.edu>
2020-02-14 13:59:34 -0500

    Got getting logged in user working

--- hyposoft/src/utils/logutils.js
+++ hyposoft/src/utils/logutils.js
@@ -1,4 +1,5 @@
 import * as firebaseutils from './firebaseutils'
+import * as userutils from './userutils'
 
 // static ASSET = 'asset'
 // static MODEL = 'model'
@@ -6,22 +7,28 @@ import * as firebaseutils from './firebaseutils'
 // static DATACENTER = 'datacenter'
 
 // only optional is objectId and objectType
-function packageLog(timestamp, objectId, objectType, action, userId) {
+function packageLog(timestamp, objectId, objectType, objectName, action, userId, userName) {
     const log = {
         timestamp: timestamp,
         objectId: objectId.trim(),
         objectType: objectType.trim(),
+        objectName: objectName.trim(),
         action: action.trim(),
-        userId: userId.trim()
+        userId: userId.trim(),
+        userName: userName.trim()
     }
     return log
 }
 
 function addLog(objectId, objectType, action) {
     const timestamp = Date.now()
-    const userId = 'useruser'
-    var log = packageLog(timestamp, objectId, objectType, action, userId)
-    firebaseutils.logsRef.add(log)
+    const userId = userutils.getLoggedInUser()
+    const objectName = 'name'
+    getUserName(userId, user => {
+        const userName = user
+        var log = packageLog(timestamp, objectId, objectType, objectName, action, userId, userName)
+        firebaseutils.logsRef.add(log)
+    })
 }
 
 function getLogs(callback) {
@@ -40,8 +47,12 @@ function getLogs(callback) {
 
 function buildLog(data) {
     var log = new Date(data.timestamp).toString() + ': '
-    log += data.userId + ' ' + data.action + ' ' + data.objectType + '.'
+    log += data.userName + ' ' + data.action + ' ' + data.objectType + '.'
     return log
+}
+
+function getUserName(id,callback) {
+    firebaseutils.usersRef.doc(id).get().then(doc => callback(doc.data().username))
 }
 
 export { addLog, getLogs }
