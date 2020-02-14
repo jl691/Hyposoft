839763a37e8abe797d811ce396932f9a0ce05dc7
Justin Havas <jah135@duke.edu>
2020-02-14 14:38:42 -0500

    Add initial object name detection #120

--- hyposoft/src/utils/logutils.js
+++ hyposoft/src/utils/logutils.js
@@ -1,10 +1,25 @@
 import * as firebaseutils from './firebaseutils'
 import * as userutils from './userutils'
 
-// static ASSET = 'asset'
-// static MODEL = 'model'
-// static RACK = 'rack'
-// static DATACENTER = 'datacenter'
+function ASSET() {
+    return 'Asset'
+}
+
+function MODEL() {
+    return 'Model'
+}
+
+function RACK() {
+    return 'Rack'
+}
+
+function USER() {
+    return 'User'
+}
+
+function DATACENTER() {
+    return 'Datacenter'
+}
 
 // only optional is objectId and objectType
 function packageLog(timestamp, objectId, objectType, objectName, action, userId, userName) {
@@ -23,12 +38,37 @@ function packageLog(timestamp, objectId, objectType, objectName, action, userId,
 function addLog(objectId, objectType, action) {
     const timestamp = Date.now()
     const userId = userutils.getLoggedInUser()
-    const objectName = 'name'
-    getUserName(userId, user => {
-        const userName = user
-        var log = packageLog(timestamp, objectId, objectType, objectName, action, userId, userName)
-        firebaseutils.logsRef.add(log)
-    })
+    var ref;
+    switch (objectType) {
+        case ASSET():
+            ref = firebaseutils.assetRef
+            break
+        case MODEL():
+            ref = firebaseutils.modelsRef
+            break
+        case RACK():
+            ref = firebaseutils.racksRef
+            break
+        case USER():
+            ref = firebaseutils.usersRef
+            break
+        case DATACENTER():
+            ref = firebaseutils.datacentersRef
+            break
+        default:
+            ref = null
+    }
+
+    if (ref != null) {
+        getObjectName(ref,objectId, objectName => {
+            getUserName(userId, userName => {
+                if (objectName && userName) {
+                    var log = packageLog(timestamp, objectId, objectType, objectName, action, userId, userName)
+                    firebaseutils.logsRef.add(log)
+                }
+            })
+        })
+    }
 }
 
 function getLogs(callback) {
@@ -47,12 +87,24 @@ function getLogs(callback) {
 
 function buildLog(data) {
     var log = new Date(data.timestamp).toString() + ': '
-    log += data.userName + ' ' + data.action + ' ' + data.objectType + '.'
+    log += data.userName + ' ' + data.action + ' ' + data.objectType + ' ' + data.objectName + '.'
     return log
 }
 
 function getUserName(id,callback) {
     firebaseutils.usersRef.doc(id).get().then(doc => callback(doc.data().username))
+    .catch( error => {
+      console.log("Error getting documents: ", error)
+      callback(null)
+    })
 }
 
-export { addLog, getLogs }
+function getObjectName(ref,id,callback) {
+    ref.doc(id).get().then(doc => callback(doc.data().modelName))
+    .catch( error => {
+      console.log("Error getting documents: ", error)
+      callback(null)
+    })
+}
+
+export { ASSET, MODEL, RACK, USER, DATACENTER, addLog, getLogs }
