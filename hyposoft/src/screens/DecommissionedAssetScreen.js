a1f73c213de35ff0867e79071be019358f75e666
Justin Havas <jah135@duke.edu>
2020-03-10 16:04:52 -0400

    Populate initial datatable for decommissioned assets

--- hyposoft/src/screens/DecommissionedAssetScreen.js
+++ hyposoft/src/screens/DecommissionedAssetScreen.js
@@ -3,16 +3,204 @@ import AppBar from '../components/AppBar'
 import BackButton from '../components/BackButton'
 import UserMenu from '../components/UserMenu'
 import { ToastsContainer, ToastsStore } from 'react-toasts'
-import { Anchor, Box, Button, Grommet, Heading, TextInput } from 'grommet'
+import { Anchor, Box, Button, DataTable, Grommet, Heading, Text, TextInput } from 'grommet'
+import {Redirect} from "react-router-dom";
 import theme from '../theme'
+import * as userutils from '../utils/userutils'
+import * as decomutils from '../utils/decommissionutils'
 
 class DecommissionedAssetScreen extends Component {
+    startAfter = null
+
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+          initialLoaded: false,
+          searchQuery: '',
+          // sortField: "",
+          // sortAscending: ""
+        }
+    }
+
+    componentDidMount() {
+        this.init()
+    }
+
+    init() {
+      decomutils.getAssets(this.startAfter, (assets, newStartAfter) => {
+          this.startAfter = newStartAfter;
+          this.setState(oldState => (
+              {...oldState, assets: assets, initialLoaded: true}
+          ))
+      })
+    }
+
+    // setSort(field) {
+    //     let newSort;
+    //     if (this.state.sortField && this.state.sortField === field) {
+    //         //reverse direction
+    //         this.setState({
+    //             sortAscending: !this.state.sortAscending
+    //         });
+    //         newSort = !this.state.sortAscending;
+    //     } else {
+    //         //start with ascending
+    //         this.setState({
+    //             sortField: field,
+    //             sortAscending: true
+    //         });
+    //         newSort = true;
+    //     }
+    //
+    //     this.startAfter = null;
+    //     this.setState({
+    //         assets: [],
+    //         initialLoaded: false
+    //     });
+    //     assetutils.getAsset((newStartAfter, assetdb, empty) => {
+    //         if(empty){
+    //             this.setState({
+    //                 initialLoaded: true
+    //             })
+    //         } else if (newStartAfter && assetdb) {
+    //             console.log("new sorted ", assetdb)
+    //             this.startAfter = newStartAfter;
+    //             this.setState({assets: assetdb, initialLoaded: true})
+    //         }
+    //     }, field, newSort)
+    // }
+
+    getTable(){
+        if(!this.state.initialLoaded){
+            return <Text>Please wait...</Text>
+        } else {
+            return <DataTable
+                step={500}
+                onMore={() => {
+                    decomutils.getAssets(this.startAfter, (assets, newStartAfter) => {
+                        this.startAfter = newStartAfter;
+                        this.setState(oldState => (
+                            {assets: this.state.assets.concat(assets)}
+                        ))
+                    },this.state.searchQuery)
+                }}
+                columns={
+                    [
+                        {
+                            property: 'date',
+                            header: <Text size='small'>Date and Time (EST)</Text>,
+                            render: datum => <Text size='small'>{datum.date}</Text>,
+                            sortable: false,
+                        },
+                        {
+                            property: 'assetId',
+                            header: <Text size='small'
+                            // onClick={() => {
+                            //     this.setSort("assetId")
+                            // }} style={{cursor: "pointer"}}
+                            >Asset ID{
+                              // this.state.sortField === 'assetId' && (this.state.sortAscending ? <FormDown /> : <FormUp />)
+                            }</Text>,
+                            primary: true,
+                            render: datum => <Text size='small'>{datum.assetId}</Text>
+                        },
+                        {
+                            property: 'model',
+                            header: <Text size='small'
+                            // onClick={() => {
+                            //     this.setSort("model")
+                            // }} style={{cursor: "pointer"}}
+                            >Model   {
+                              // this.state.sortField === 'model' && (this.state.sortAscending ? <FormDown /> : <FormUp />)
+                            }</Text>,
+                            render: datum => <Text size='small'>{datum.model}</Text>,
+                        },
+                        {
+                            property: 'hostname',
+                            header: <Text size='small'
+                            // onClick={() => {
+                            //     this.setSort("hostname")
+                            // }} style={{cursor: "pointer"}}
+                            >Hostname  {
+                              // this.state.sortField === 'hostname' && (this.state.sortAscending ? <FormDown /> : <FormUp />)
+                            }</Text>,
+                            render: datum => <Text wordBreak="break-all"size='small'>{datum.hostname}</Text>,
+                        },
+                        {
+                            property: 'rack',
+                            header: <Text size='small'
+                            // onClick={() => {
+                            //     this.setSort("rack")
+                            // }} style={{cursor: "pointer"}}
+                            >Rack  {
+                              // this.state.sortField === 'rack' && (this.state.sortAscending ? <FormDown /> : <FormUp />)
+                            }</Text>,
+                            render: datum => <Text size='small'>{datum.rack}</Text>,
+                        },
+                        {
+                            property: 'rackU',
+                            header: <Text size='small'
+                            // onClick={() => {
+                            //     this.setSort("rackU")
+                            // }} style={{cursor: "pointer"}}
+                            >Rack U  {
+                              // this.state.sortField === 'rackU' && (this.state.sortAscending ? <FormDown /> : <FormUp />)
+                            }</Text>,
+                            render: datum => <Text size='small'>{datum.rackU}</Text>,
+                        },
+                        {
+                            property: 'owner',
+                            header: <Text size='small'
+                            // onClick={() => {
+                            //     this.setSort("owner")
+                            // }} style={{cursor: "pointer"}}
+                            >Owner  {
+                              // this.state.sortField === 'owner' && (this.state.sortAscending ? <FormDown /> : <FormUp />)
+                            }</Text>,
+                            render: datum => <Text size='small'>{datum.owner}</Text>,
+                        },
+                        {
+                            property: 'datacenterAbbrev',
+                            header: <Text size='small'
+                            // onClick={() => {
+                            //     this.setSort("datacenterAbbrev")
+                            // }} style={{cursor: "pointer"}}
+                            > Datacenter Abbrev.  {
+                              // this.state.sortField === 'datacenterAbbrev' && (this.state.sortAscending ? <FormDown /> : <FormUp />)
+                            }</Text>,
+                            render: datum => <Text size='small'>
+                                {datum.datacenterAbbrev}
+                            </Text>,
+                        },
+                    ]
+                }
+                data={this.state.assets}
+                sortable={true}
+                size="medium"
+                // Decommissioned detail view
+                // onClickRow={({datum}) => {
+                //     logutils.doesObjectStillExist(datum.objectType,datum.objectId,exists => {
+                //         if (exists) {
+                //             if (datum.objectType === logutils.MODEL()) {
+                //                 this.props.history.push('/models/'+datum.currentData.vendor+'/'+datum.currentData.modelNumber)
+                //             } else if (datum.objectType === logutils.ASSET()) {
+                //                 this.props.history.push('/assets/'+datum.objectId)
+                //             } else {
+                //                 ToastsStore.error(datum.objectType+' does not have a detailed view', 3000)
+                //             }
+                //         } else {
+                //             ToastsStore.error(datum.objectType+' does not exist anymore', 3000)
+                //         }
+                //     })
+                // }}
+            />
+        }
     }
 
     render() {
+        if (!userutils.isUserLoggedIn()) {
+            return <Redirect to='/'/>
+        }
         return (
           <Grommet theme={theme} full className='fade'>
               <Box fill background='light-2'>
@@ -23,7 +211,40 @@ class DecommissionedAssetScreen extends Component {
                       }} >Decommissioned Assets</Heading>
                       <UserMenu alignSelf='end' this={this} />
                   </AppBar>
+
+                  <Box direction='row'
+                      justify='center'
+                      wrap={true}>
+                      <Box direction='row' justify='center'>
+                             <Box direction='row' justify='center'>
+                                 <Box width='xlarge' direction='column' align='stretch' justify='start'>
+
+                                     <Box style={{
+                                              borderRadius: 10,
+                                              borderColor: '#EDEDED'
+                                          }}
+                                         id='containerBox'
+                                         direction='row'
+                                         background='#FFFFFF'
+                                         margin={{top: 'medium', bottom: 'medium'}}
+                                         flex={{
+                                             grow: 0,
+                                             shrink: 0
+                                         }}
+                                         pad='small' >
+                                         <Box margin={{left: 'medium', top: 'small', bottom: 'small', right: 'medium'}} direction='column'
+                                             justify='start' alignSelf='stretch' flex>
+                                             <Box align="center">
+                                                 {this.getTable()}
+                                              </Box>
+                                         </Box>
+                                     </Box>
+                                 </Box>
+                             </Box>
+                         </Box>
+                     </Box>
               </Box>
+              <ToastsContainer store={ToastsStore}/>
           </Grommet>
         )
     }
--- hyposoft/src/utils/decommissionutils.js
+++ hyposoft/src/utils/decommissionutils.js
@@ -27,4 +27,26 @@ function decommissionAsset(id,callback) {
     })
 }
 
-export { decommissionAsset }
+function getAssets(startAfter, callback, search = '') {
+    var query = startAfter ? firebaseutils.decommissionRef.orderBy('timestamp','desc').startAfter(startAfter)
+                           : firebaseutils.decommissionRef.orderBy('timestamp','desc')
+    query.get().then(docSnaps => {
+        var newStartAfter = docSnaps.docs[docSnaps.docs.length-1]
+
+        const assets = docSnaps.docs.map(doc => (
+            {...doc.data(), date: getDate(doc.data().timestamp)}
+        ))
+        callback(assets,newStartAfter)
+    })
+    .catch( error => {
+        console.log("Error getting documents: ", error)
+        callback(null,null)
+    })
+}
+
+function getDate(timestamp) {
+    var dateArray = new Date(timestamp).toString().split(' ',5)
+    return dateArray.join(' ')
+}
+
+export { decommissionAsset, getAssets }
