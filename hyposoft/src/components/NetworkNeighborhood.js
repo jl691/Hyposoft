d79feabbad73485e6b54c5536b5bf7bf22958a53
Justin Havas <jah135@duke.edu>
2020-03-11 19:37:58 -0400

    Save graph when decommissioning and modify network graph to take in this graph

--- hyposoft/src/components/NetworkNeighborhood.js
+++ hyposoft/src/components/NetworkNeighborhood.js
@@ -3,6 +3,7 @@ import theme from "../theme";
 import {Box, Grommet, Heading} from "grommet";
 import cytoscape from "cytoscape";
 import * as assetnetworkportutils from '../utils/assetnetworkportutils'
+import * as decomutils from '../utils/decommissionutils'
 import BackButton from "./BackButton";
 import UserMenu from "./UserMenu";
 import AppBar from "./AppBar";
@@ -10,77 +11,89 @@ import AppBar from "./AppBar";
 class NetworkNeighborhood extends React.Component {
 
     componentDidMount() {
-        let data;
         console.log("here")
-        assetnetworkportutils.getNetworkPortConnections(this.props.match.params.assetID, result => {
-            console.log(result)
-            if(result){
-                console.log(result);
-                data = result;
-                let cy = cytoscape({
-                    container: document.getElementById("network"),
-                    elements: data,
-
-                    style: [ // the stylesheet for the graph
-                        {
-                            selector: '.origin',
-                            style: {
-                                'background-color': '#4BBD51',
-                                'label': 'data(display)',
-                                "font-size": "8px",
-                                "text-wrap": "wrap"
-                            }
-                        },
-                        {
-                            selector: '.second',
-                            style: {
-                                'background-color': '#395E66',
-                                'label': 'data(display)',
-                                "font-size": "8px",
-                                "text-wrap": "wrap"
-                            }
-                        },
-                        {
-                            selector: '.third',
-                            style: {
-                                'background-color': '#B8C4BB',
-                                'label': 'data(display)',
-                                "font-size": "8px",
-                                "text-wrap": "wrap"
-                            }
-                        },
-                        {
-                            selector: 'edge',
-                            style: {
-                                'width': 3,
-                                'line-color': '#ccc',
-                                'target-arrow-color': '#ccc',
-                                'target-arrow-shape': 'triangle'
-                            }
-                        }
-                    ],
-
-                    layout: {
-                        name: 'concentric',
-                        concentric: function( node ){
-                            console.log(node.data().level)
-                            return (3-node.data().level);
-                        },
-                        fit: true,
-                        minNodeSpacing: 30
-                    }
-                });
-                cy.on('tap', 'node', function(){
-                    try { // your browser may block popups
-                        window.open( "/assets/" + this.data('id') );
-                    } catch(e){ // fall back on url change
-                        window.location.href = "/assets/" + this.data('id');
-                    }
-                });
-            } else {
-                document.getElementById("network").innerHTML = "No network connections."
+        decomutils.getAssetDetails(this.props.match.params.assetID, asset => {
+            if (asset && asset.graph) {
+                this.buildGraph(asset.graph,false)
+                return
             }
-        });
+            assetnetworkportutils.getNetworkPortConnections(this.props.match.params.assetID, result => {
+                console.log(result)
+                this.buildGraph(result,true)
+            });
+        })
+    }
+
+    buildGraph(result,deployed) {
+      let data;
+      if(result){
+          console.log(result);
+          data = result;
+          let cy = cytoscape({
+              container: document.getElementById("network"),
+              elements: data,
+
+              style: [ // the stylesheet for the graph
+                  {
+                      selector: '.origin',
+                      style: {
+                          'background-color': '#4BBD51',
+                          'label': 'data(display)',
+                          "font-size": "8px",
+                          "text-wrap": "wrap"
+                      }
+                  },
+                  {
+                      selector: '.second',
+                      style: {
+                          'background-color': '#395E66',
+                          'label': 'data(display)',
+                          "font-size": "8px",
+                          "text-wrap": "wrap"
+                      }
+                  },
+                  {
+                      selector: '.third',
+                      style: {
+                          'background-color': '#B8C4BB',
+                          'label': 'data(display)',
+                          "font-size": "8px",
+                          "text-wrap": "wrap"
+                      }
+                  },
+                  {
+                      selector: 'edge',
+                      style: {
+                          'width': 3,
+                          'line-color': '#ccc',
+                          'target-arrow-color': '#ccc',
+                          'target-arrow-shape': 'triangle'
+                      }
+                  }
+              ],
+
+              layout: {
+                  name: 'concentric',
+                  concentric: function( node ){
+                      console.log(node.data().level)
+                      return (3-node.data().level);
+                  },
+                  fit: true,
+                  minNodeSpacing: 30
+              }
+          });
+          if (deployed) {
+            cy.on('tap', 'node', function(){
+                try { // your browser may block popups
+                    window.open( "/assets/" + this.data('id') );
+                } catch(e){ // fall back on url change
+                    window.location.href = "/assets/" + this.data('id');
+                }
+            });
+          }
+      } else {
+          document.getElementById("network").innerHTML = "No network connections."
+      }
     }
 
     render() {
--- hyposoft/src/utils/decommissionutils.js
+++ hyposoft/src/utils/decommissionutils.js
@@ -1,6 +1,7 @@
 import * as firebaseutils from './firebaseutils'
 import * as logutils from './logutils'
 import * as userutils from './userutils'
+import * as assetnetworkportutils from '../utils/assetnetworkportutils'
 
 function decommissionAsset(id,callback) {
     firebaseutils.assetRef.doc(id).get().then(doc => {
@@ -9,24 +10,26 @@ function decommissionAsset(id,callback) {
             return
         }
         const docData = doc.data()
-        firebaseutils.usersRef.doc(userutils.getLoggedInUser()).get().then(doc => {
-          if (!doc.exists) {
+        assetnetworkportutils.getNetworkPortConnections(id, graph => {
+          firebaseutils.usersRef.doc(userutils.getLoggedInUser()).get().then(doc => {
+            if (!doc.exists) {
+                callback(false)
+                return
+            }
+            firebaseutils.assetRef.doc(id).delete().then(() => {
+                logutils.addLog(id,logutils.ASSET(),logutils.DECOMMISSION(),docData)
+                firebaseutils.decommissionRef.add({...docData,timestamp: Date.now(),name: doc.data().username,graph: graph}).then(() => callback(true))
+                .catch( error => {
+                    console.log("Error getting documents: ", error)
+                    callback(false)
+                })
+                })
+            })
+            .catch( error => {
+              console.log("Error getting documents: ", error)
               callback(false)
-              return
-          }
-          firebaseutils.assetRef.doc(id).delete().then(() => {
-              logutils.addLog(id,logutils.ASSET(),logutils.DECOMMISSION(),docData)
-              firebaseutils.decommissionRef.add({...docData,timestamp: Date.now(),name: doc.data().username}).then(() => callback(true))
-              .catch( error => {
-                  console.log("Error getting documents: ", error)
-                  callback(false)
-              })
-              })
-          })
-          .catch( error => {
-            console.log("Error getting documents: ", error)
-            callback(false)
-          })
+            })
+        })
     })
     .catch( error => {
       console.log("Error getting documents: ", error)
