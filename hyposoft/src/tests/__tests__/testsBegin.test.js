6f3a2b0b10f849a3e02c5f9c03fd2add9e6fff58
Justin Havas <jah135@duke.edu>
2020-03-22 23:37:21 -0400

    Add some tests to import

--- hyposoft/src/tests/__tests__/testsBegin.test.js
+++ hyposoft/src/tests/__tests__/testsBegin.test.js
@@ -1,4 +1,7 @@
-
+import {modelutilsTest} from '../modelutilsTest.js'
+import {decomutilsTest} from '../decomutilsTest.js'
+import {changeplanconflictPowerConnsTest} from '../changeplanconflictPowerConnsTest.js'
+import {changeplanconflictNetworkConnsTest} from '../changeplanconflictNetworkConnsTest.js'
 
 describe('Begin All Tests', () => {
 
