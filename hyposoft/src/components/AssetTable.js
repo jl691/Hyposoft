d95e7237896c7d600c3a5d13e8dcaf90ca50a2de
Allen Qiu <asq3@duke.edu>
2020-03-24 21:06:23 -0400

    tooltips for power options

--- hyposoft/src/components/AssetTable.js
+++ hyposoft/src/components/AssetTable.js
@@ -7,6 +7,7 @@ import * as assetmacutils from '../utils/assetmacutils'
 import * as powerutils from '../utils/powerutils'
 import * as userutils from "../utils/userutils";
 import * as assetnetworkportutils from '../utils/assetnetworkportutils'
+import ReactTooltip from 'react-tooltip'
 
 export default class AssetTable extends Component {
 
@@ -330,6 +331,7 @@ export default class AssetTable extends Component {
             //                     <Box margin={{ left: 'medium', top: 'small', bottom: 'small', right: 'medium' }} direction='column'
             //                         justify='start' alignSelf='stretch' flex >
             //                         <Box align="center" >
+            <Box>
             <DataTable
                 step={25}
                 onMore={() => {
@@ -457,7 +459,7 @@ export default class AssetTable extends Component {
 
                                 if ((userutils.isLoggedInUserAdmin() || userutils.getLoggedInUserUsername() === datum.owner) && datum.datacenterAbbrev.toUpperCase() === "RTP1" && datum.rackRow.charCodeAt(0) >= 65 && datum.rackRow.charCodeAt(0) <= 69 && parseInt(datum.rackNum) >= 1 && parseInt(datum.rackNum) <=19 && datum.powerConnections && datum.powerConnections.length) {
                                 return (<Box direction={"row"} justify={"center"}>
-                                    <Power style={{backgroundColor: this.colors[datum.asset_id+'_on_color']}} onClick={(e) => {
+                                    <Power data-tip="Power on" style={{backgroundColor: this.colors[datum.asset_id+'_on_color']}} onClick={(e) => {
                                         e.persist()
                                         e.nativeEvent.stopImmediatePropagation()
                                         e.stopPropagation()
@@ -492,7 +494,7 @@ export default class AssetTable extends Component {
                                     }
                                            onMouseOver={e => this.colors[datum.asset_id+'_on_color']='#dddddd'}
                                            onMouseLeave={e => this.colors[datum.asset_id+'_on_color']=''}/>
-                                    <Clear style={{backgroundColor: this.colors[datum.asset_id+'_off_color']}} onClick={(e) => {
+                                    <Clear data-tip="Power off" style={{backgroundColor: this.colors[datum.asset_id+'_off_color']}} onClick={(e) => {
                                         e.persist()
                                         e.nativeEvent.stopImmediatePropagation()
                                         e.stopPropagation()
@@ -527,7 +529,7 @@ export default class AssetTable extends Component {
                                     }
                                            onMouseOver={e => this.colors[datum.asset_id+'_off_color']='#dddddd'}
                                            onMouseLeave={e => this.colors[datum.asset_id+'_off_color']=''}/>
-                                    <PowerCycle style={{backgroundColor: this.colors[datum.asset_id+'_cycle_color']}} onClick={(e) => {
+                                    <PowerCycle data-tip="Power cycle" style={{backgroundColor: this.colors[datum.asset_id+'_cycle_color']}} onClick={(e) => {
                                         e.persist()
                                         e.nativeEvent.stopImmediatePropagation()
                                         e.stopPropagation()
@@ -593,7 +595,7 @@ export default class AssetTable extends Component {
                                                 onMouseLeave={e => this.colors[datum.asset_id+'_cycle_color']=''}/>
                                 </Box>)
                             } else {
-                                return (<Text size={"small"}>No options available.</Text>)
+                                return (<Text size={"small"} data-tip="No power options available">-</Text>)
                             }
                         }
                     },
@@ -618,6 +620,8 @@ export default class AssetTable extends Component {
 
 
             />
+                <ReactTooltip />
+            </Box>
             //                         </Box>
             //                     </Box>
             //                 </Box>
