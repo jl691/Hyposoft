c057f1d5e1376167204679cabd330600c5efda02
Justin Havas <jah135@duke.edu>
2020-04-11 18:17:46 -0400

    Added tests for updating a chassis

--- hyposoft/src/__tests__/bladeutils.test.js
+++ hyposoft/src/__tests__/bladeutils.test.js
@@ -13,7 +13,7 @@ describe('bladeutilsTest', () => {
     })
   })
 
-  describe('chassisTests', () => {
+  describe('addChassisTests', () => {
     test('addChassis valid asset', done => {
       ids = {...ids,chassis: '999990'}
       bladeutils.addChassis(ids['chassis'], 'Dell man5', 'chassisHost', 'A1', 42, '', '', 'Test Datacenter',
@@ -40,6 +40,54 @@ describe('bladeutilsTest', () => {
             expect(docRef.data().objectId).toBe('999990')
             done()
           })
+      })
+    })
+
+    // cleanup log
+    afterAll(done => {
+      firebaseutils.logsRef.doc(ids['log']).delete().then(docRef => {
+          done()
+      })
+    })
+  })
+
+  describe('updateChassisTests', () => {
+    test('updateChassis valid asset', done => {
+      bladeutils.updateChassis(ids['chassis'], 'Dell man5', 'chassisHostname', 'C24', 10, '', '', 'Test Datacenter',
+        {}, [], [], [], '', '', '', '',
+        result => {
+          expect(result).toBe(null)
+          done()
+      })
+    })
+
+    // add test checking servers inside were updated
+
+    test('updateChassis blades collection doc exists and was properly updated', done => {
+      firebaseutils.db.collectionGroup('blades').where('id','==',ids['chassis']).get().then(qs => {
+        expect(!qs.empty).toBe(true)
+        expect(qs.docs[0].ref.parent.parent.id).toBe(ids['rack2'])
+        expect(qs.docs[0].data().letter).toBe('chassisHostname')
+        done()
+      })
+    })
+
+    test('updateChassis log was added', done => {
+      firebaseutils.logsRef.get().then(docSnaps => {
+          // change this!!!
+          ids = {...ids,log: docSnaps.docs[0].id}
+          firebaseutils.logsRef.doc(ids['log']).get().then(docRef => {
+            expect(docRef.data().action).toBe('modified')
+            expect(docRef.data().objectId).toBe('999990')
+            done()
+          })
+      })
+    })
+
+    // cleanup log
+    afterAll(done => {
+      firebaseutils.logsRef.doc(ids['log']).delete().then(docRef => {
+          done()
       })
     })
   })
@@ -94,31 +142,36 @@ function addInitialAssets(callback) {
   const dc = makeDatacenter()
   firebaseutils.datacentersRef.add(dc).then(docRef => {
     ids = {...ids,datacenter: docRef.id}
-    const rack = makeRack()
+    const rack = makeRack('A',1)
     firebaseutils.racksRef.add(rack).then(docRef => {
       ids = {...ids,rack: docRef.id}
+      const rack2 = makeRack('C',24)
+      firebaseutils.racksRef.add(rack2).then(docRef => {
+        ids = {...ids,rack2: docRef.id}
       const dell = makeModel('Dell','man5',1,'chassis')
       firebaseutils.modelsRef.add(dell).then(docRef => {
         ids = {...ids,model: docRef.id}
         callback()
+      })
       })
     })
   })
 }
 
 function tearDownAssets(callback) {
-  // come back to here!!!
   firebaseutils.modelsRef.doc(ids['model']).delete().then(docRef => {
     firebaseutils.assetRef.doc(ids['chassis']).delete().then(docRef => {
       firebaseutils.datacentersRef.doc(ids['datacenter']).delete().then(docRef => {
         firebaseutils.db.collectionGroup('blades').where('id','==',ids['chassis']).get().then(qs => {
             qs.docs[0].ref.delete().then(docRef => {
               firebaseutils.racksRef.doc(ids['rack']).delete().then(docRef => {
+                firebaseutils.racksRef.doc(ids['rack2']).delete().then(docRef => {
                     // firebaseutils.decommissionRef.doc(ids['decom']).delete().then(docRef => {
-                      firebaseutils.logsRef.doc(ids['log']).delete().then(docRef => {
+                      // firebaseutils.logsRef.doc(ids['log']).delete().then(docRef => {
                           callback()
-                        })
+                        // })
                       // })
+                    })
                     })
             })
         })
@@ -155,10 +208,10 @@ function makeDatacenter() {
   return dc
 }
 
-function makeRack() {
+function makeRack(letter,number) {
   const rack = {
-    letter: 'A',
-    number: 1,
+    letter: letter,
+    number: number,
     height: 42,
     assets: [],
     powerPorts:[],
